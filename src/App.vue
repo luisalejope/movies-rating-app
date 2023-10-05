baf6e1451e2a7cb44e85c4e29b0537c35c9274ee
Luis Alejandro Peña <luisalejandropena425@gmail.com>
2023-10-04 22:59:32 -0500

    movies-rating-app: first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { useLoaderStore } from '@/stores/loader';
+
+import GenericLoader from '@/components/global/GenericLoader.vue';
+import NavBar from '@/components/NavBar.vue'
+
+
+// STORES
+const loadStore = useLoaderStore();
+
+const { getLoaderStatus } = storeToRefs(loadStore);
+
+</script>
+
+<template>
+  <div id="poc-container" class="mb-xl">
+    <NavBar />
+    <RouterView />
+    <GenericLoader v-show="getLoaderStatus" />
+  </div>
+</template>
+
+<style scoped>
+#poc-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
